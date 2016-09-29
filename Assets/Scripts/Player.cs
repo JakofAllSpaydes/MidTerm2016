1d0ccfa081bcc2e2d508549760938a64dc807b39
Jack Guo <yiming7391@gmail.com>
2016-09-29 16:08:53 -0400

    Initial Commit - Basic movement and left right camera mouse movement implemented. Some basic shapes on the floor.

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	CharacterController cController;
+
+	// Use this for initialization
+	void Start () {
+	
+		cController = GetComponent<CharacterController>();
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		float inputX = Input.GetAxis ("Horizontal"); // A/D, LeftArrow/RightArrow
+		float inputY = Input.GetAxis ("Vertical"); // W/S, UpArrow/DownArrow
+		float mouseX = Input.GetAxis ("Mouse X"); //Mouse X is the current horizontal mouse speed
+		float mouseY = Input.GetAxis ("Mouse Y");
+
+		//movement
+		cController.SimpleMove (transform.forward * inputY * 5f);
+		cController.SimpleMove (transform.right * inputX * 5f);
+
+		transform.Rotate (0f, mouseX * 0.5f, 0f);
+		//transform.Rotate (mouseY * 0.2f, 0f, 0f);
+
+	}
+}
