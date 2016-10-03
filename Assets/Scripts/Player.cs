0910119d3516787fcf5381667bd482e123792272
Jack Guo <yiming7391@gmail.com>
2016-10-03 18:41:17 -0400

    1st attempt at picking up materials code. Most of the frameworks have been laid down, doesn't really work yet though. Invisible trigger works, but the subsequent Keypress doesn't.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,10 +5,22 @@ public class Player : MonoBehaviour {
 
 	CharacterController cController;
 
+	public GameObject Mat1;
+	public GameObject Mat2;
+	public GameObject Mat1Ghost;
+	public GameObject Mat2Ghost;
+	public GameObject Mat1Pan;
+	public GameObject Mat2Pan;
+
+	public bool HoldingMat;
+	public float MatNum;
+	public int CorrectMat;
+
 	// Use this for initialization
 	void Start () {
 	
 		cController = GetComponent<CharacterController>();
+		HoldingMat = false;
 
 
 	}
@@ -28,5 +40,55 @@ public class Player : MonoBehaviour {
 		transform.Rotate (0f, mouseX * 0.5f, 0f);
 		//transform.Rotate (mouseY * 0.2f, 0f, 0f);
 
+	}
+
+	void OnTriggerEnter(Collider myTrigger) {
+		if (myTrigger.gameObject.name == "mat1trigger") {
+			Debug.Log ("mat1triggered");
+
+			if (Input.GetKeyDown (KeyCode.Space) && HoldingMat == false) {
+				Debug.Log ("Key Hit");
+				
+				Mat1.GetComponent<Renderer> ().enabled = false;
+				Mat1Ghost.GetComponent<Renderer> ().enabled = true;
+
+				HoldingMat = true;
+				MatNum = 1;
+			}
+
+		}
+
+		if (myTrigger.gameObject.name == "mat2trigger") {
+			Debug.Log ("mat2triggered");
+
+			if (Input.GetKeyDown (KeyCode.Space) && HoldingMat == false) {
+				
+				Mat2.GetComponent<Renderer> ().enabled = false;
+				Mat2Ghost.GetComponent<Renderer> ().enabled = true;
+
+				HoldingMat = true;
+				MatNum = 2;
+			}
+
+		}
+
+		if (myTrigger.gameObject.name == "panTrigger") {
+			Debug.Log ("panTriggered");
+
+			if (Input.GetKeyDown (KeyCode.Space)) {
+				if (MatNum == 1) {
+					Mat1Pan.GetComponent<Renderer> ().enabled = true;
+
+					MatNum = MatNum - 1;
+				}
+
+				if (MatNum == 2) {
+					Mat2Pan.GetComponent<Renderer> ().enabled = true;
+
+					MatNum = MatNum - 2;
+				}
+
+			}
+		}
 	}
 }
